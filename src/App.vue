8b532e7896d501c905974f9967475e1baedb9de6
0xa1d1 <0xa1d1+git@pm.me>
2024-06-29 11:12:22 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import WebApp from '@twa-dev/sdk'
-
-WebApp.ready();
-</script>
-
 <template>
   <div>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <button @click="openModal" id="connect">Launch a TON Jetton</button>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<script setup>
+import WebApp from '@twa-dev/sdk';
+import {TonConnectUI} from '@tonconnect/ui';
+
+const tonConnectUI = new TonConnectUI({
+  buttonRootId: 'connect',
+  manifestUrl: 'https://0xa1d1.github.io/tongate/tonconnect-manifest.json'
+});
+
+WebApp.ready();
+
+const openModal = async () => {
+  try {
+    await tonConnectUI.openModal();
+  } catch (error) {
+    console.error('Failed to open modal:', error);
+  }
+};
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+button {
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+button:hover {
+  background-color: #0056b3;
 }
 </style>
