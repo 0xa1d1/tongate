29036ba6764ccd37d36db6ae34c855db1eecb120
0xa1d1 <0xa1d1+git@pm.me>
2024-06-29 11:41:56 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,48 @@
 <template>
   <div>
-    <button @click="openModal" id="connect">Connect Wallet</button>
+    <button @click="openModal" v-if="!connected">Connect Wallet</button>
+    <div v-else>
+      <div>{{ address }}</div><br>
+      <button @click="disconnect">Disconnect</button>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import WebApp from '@twa-dev/sdk';
-import {TonConnectUI} from '@tonconnect/ui';
+import { TonConnectUI, toUserFriendlyAddress } from '@tonconnect/ui';
+
+WebApp.ready();
 
 const tonConnectUI = new TonConnectUI({
   manifestUrl: 'https://0xa1d1.github.io/tongate/tonconnect-manifest.json'
 });
 
-WebApp.ready();
+const connected = ref(tonConnectUI.connected);
+const address = ref("");
+
+const updateAddress = (info) => {
+  if (info?.account?.address) {
+    address.value = toUserFriendlyAddress(info.account.address)
+  } else {
+    address.value = "";
+  }
+}
+
+const unsubscribe = tonConnectUI.onStatusChange(
+  walletAndwalletInfo => {
+    connected.value = tonConnectUI.connected;
+    if (tonConnectUI.connected) {
+      updateAddress(walletAndwalletInfo);
+    }
+  }
+);
+
+
+const disconnect = async () => {
+  await tonConnectUI.disconnect();
+}
 
 const openModal = async () => {
   try {
